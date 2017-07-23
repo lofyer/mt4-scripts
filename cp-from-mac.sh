d6b9d8a820384e450a4ddb7bc71f957ab6fc3807
lofyer <lofyer@gmail.com>
2017-07-24 01:51:59 +0800

    delete bash file

--- cp-from-mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR=(Experts Files Images Include Indicators Libraries Logs Projects Scripts)
-for i in ${DIR[@]}
-do
-    rsync -avrPS --delete ~/Library/Application\ Support/CrossOver/Bottles/MetaTrader\ 4/drive_c/Program\ Files/OANDA\ -\ MetaTrader/mql4/$i .
-done
--- cp-from-notebook.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avrPS --delete /mnt/c/Users/jiang/AppData/Roaming/MetaQuotes/Terminal/3212703ED955F10C7534BE8497B221F4/MQL4/* .
